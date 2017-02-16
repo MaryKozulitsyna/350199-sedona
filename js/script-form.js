b7c345cd1fc9cd0c54676eaf8931554279cd8be8
MaryKozulitsyna <mary811@mail.ru>
2017-02-16 16:02:41 +0300

    Исправлено при переполнении контентом и единообразие написания.

--- js/script-form.js
+++ js/script-form.js
@@ -8,16 +8,18 @@ button.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("appointment-form-close");
 });
+
 popup.addEventListener("submit", function(event) {
   if (!checkin.value || !departure.value || !adults.value) {
-  event.preventDefault();
-  alert("Нужно ввести даты заезда и выезда, и количество людей");
+    event.preventDefault();
+    alert("Нужно ввести даты заезда и выезда, и количество людей");
   }
 });
+
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
-  if (popup.classList.contains("appointment-form")) {
-  popup.classList.toggle("appointment-form-close");
-}
-}
+    if (popup.classList.contains("appointment-form")) {
+      popup.classList.toggle("appointment-form-close");
+    }
+  }
 });
